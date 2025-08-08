1abbe5646199fdfda60a8cd3e54d69e782638421
Alison <alisonalvesmota@gmail.com>
2025-08-08 19:17:35 -0300

    Corrigindo problema de 404 quando tenta acessar uma página diferente de /home

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -7,21 +7,37 @@ import Booking from './pages/Booking';
 import Contact from './pages/Contact';
 import Player from './pages/Player';
 
+const RedirectHandler = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const redirectPath = sessionStorage.redirect;
+        delete sessionStorage.redirect;
+
+        if (redirectPath && redirectPath !== window.location.pathname) {
+            navigate(redirectPath);
+        }
+    }, [navigate]);
+
+    return null;
+};
+
 function App() {
-  return (
-    <Router>
-      <div className="relative">
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/booking" element={<Booking />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/player" element={<Player />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <Router>
+            <div className="relative">
+                <Navigation />
+                <RedirectHandler />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/booking" element={<Booking />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/player" element={<Player />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
