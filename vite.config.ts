4d42af7f06d8507423a0e2a431ff399ef98fa026
Alison <alisonalvesmota@gmail.com>
2025-08-08 18:46:27 -0300

    deploy atualizado

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: "https:forrodohorizonte.com.br/",
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-});
+    base: '/site-forro-do-horizonte/',
+    plugins: [react()],
+})
