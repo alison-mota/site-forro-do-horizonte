2f1dd3d61e34362dec6d3dcd750a15416e43f2c3
Alison <alisonalvesmota@gmail.com>
2025-08-08 19:08:27 -0300

    Tentando resolver problema de deploy.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/site-forro-do-horizonte/',
+    base: '/',
     plugins: [react()],
 })
